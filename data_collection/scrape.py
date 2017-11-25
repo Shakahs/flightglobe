bc92c82a98f62e072179cfa946c786ffad4c5553
Shakeel Saleemi <shakahs@gmail.com>
2017-11-24 20:35:54 -0800

    Update data scraper to run forever, handle ID conflict correctly

--- data_collection/scrape.py
+++ data_collection/scrape.py
@@ -1,9 +1,14 @@
 import requests
 import rethinkdb as r
-
-data = requests.get('http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json').json()
-validData=filter(lambda ac: {'Icao','Lat','Long'} <= set(ac) , data['acList'])
-res=map(lambda ac: {'id':ac['Icao'], 'lat':ac['Lat'], 'lon': ac['Long']},validData)
+import time
 
 conn=r.connect('localhost', 28015)
-r.db('flightglobe').table('flight_position').insert(res).run(conn)
+
+while(True):
+    data = requests.get('http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json').json()
+    validData=filter(lambda ac: {'Icao','Lat','Long'} <= set(ac) , data['acList'])
+    finalData=map(lambda ac: {'id':ac['Icao'], 'lat':ac['Lat'], 'lon': ac['Long']},validData)
+    result = r.db('flightglobe').table('flight_position').insert(finalData,conflict="replace").run(conn)
+    print(result)
+    time.sleep(5)
+
