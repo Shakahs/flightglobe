de2daba66ea431a0581251ed26cb68a1613c5efd
Shakeel Saleemi <shakahs@gmail.com>
2017-11-25 01:58:37 -0800

    Add altitude and heading to data collection

--- data_collection/scrape.py
+++ data_collection/scrape.py
@@ -4,11 +4,12 @@ import time
 
 conn=r.connect('localhost', 28015)
 
-
-def scrapeADBS():
+def scrapeADSB():
     rawData = requests.get('http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json').json()
-    validData=filter(lambda ac: {'Icao','Lat','Long','PosTime'} <= set(ac) , rawData['acList'])
-    finalData=map(lambda ac: {'id':ac['Icao'], 'lat':ac['Lat'], 'lon': ac['Long'], 'time': int(str(ac['PosTime'])[:-3])},validData)
+    validData=filter(lambda ac: {'Icao','Lat','Long','PosTime','Trak','Galt'} <= set(ac) , rawData['acList'])
+    finalData=map(lambda ac: {'id':ac['Icao'], 'lat':ac['Lat'], 'lon': ac['Long'],
+                              'time': int(str(ac['PosTime'])[:-3]), 'heading': ac['Trak'],
+                              'altitude': float(ac['Galt'])/3.28},validData)
     return finalData
 
 openSkyFields=['icao24','callsign','origin_country','time_position','last_contact','longitude','latitude',
@@ -17,8 +18,11 @@ openSkyFields=['icao24','callsign','origin_country','time_position','last_contac
 def scrapeOpenSky():
     rawData = requests.get('https://opensky-network.org/api/states/all').json()
     mappedData=map(lambda single: {k[1]: single[k[0]] for k in enumerate(openSkyFields)}, rawData['states'])
-    validData = filter(lambda ac: all([ac['icao24'], ac['latitude'], ac['longitude'], ac['time_position']]), mappedData)
-    finalData = map(lambda ac: {'id': ac['icao24'].upper(), 'lat': ac['latitude'], 'lon': ac['longitude'], 'time': ac['time_position']}, validData)
+    validData = filter(lambda ac: all([ac['icao24'], ac['latitude'], ac['longitude'], ac['time_position'],
+                                       ac['heading'], ac['baro_altitude']]), mappedData)
+    finalData = map(lambda ac: {'id': ac['icao24'].upper(), 'lat': ac['latitude'], 'lon': ac['longitude'],
+                                'time': ac['time_position'], 'heading': ac['heading'], 'altitude': ac['baro_altitude']},
+                    validData)
     return finalData
 
 def merge(a,b):
@@ -30,9 +34,9 @@ def merge(a,b):
     return mergedData
 
 while(True):
-    adbsresult = scrapeADBS()
+    adsbresult = scrapeADSB()
     osresult = scrapeOpenSky()
-    mergedData = merge(adbsresult, osresult)
+    mergedData = merge(adsbresult, osresult)
     insertResult = r.db('flightglobe').table('flight_position').insert(mergedData.values(), conflict="replace").run(conn)
     print(insertResult)
     time.sleep(10)
