893050c1f38efe3f169914ec89bbce8362ba59b7
Shakeel Saleemi <shakahs@gmail.com>
2017-11-25 02:42:08 -0800

    Planes now displaying with headings, although they all seem to be wrong..

--- client-source/js/components/cesium_components/primitives/CesiumEntity.jsx
+++ client-source/js/components/cesium_components/primitives/CesiumEntity.jsx
@@ -1,10 +1,9 @@
 import { Component } from 'react';
 
 import Cartesian3 from 'cesium/Source/Core/Cartesian3';
-import HorizontalOrigin from 'cesium/Source/Scene/HorizontalOrigin';
-import VerticalOrigin from 'cesium/Source/Scene/VerticalOrigin';
-import DistanceDisplayCondition from 'cesium/Source/Core/DistanceDisplayCondition';
-
+import HeadingPitchRoll from 'cesium/Source/Core/HeadingPitchRoll';
+import CesiumMath from 'cesium/Source/Core/Math';
+import Transforms from 'cesium/Source/Core/Transforms';
 
 import { shallowEqual } from '../../../utils/utils';
 
@@ -14,9 +13,14 @@ export default class CesiumEntity extends Component {
     const { planeData } = this.props;
     this.entity = planeData.entities.getOrCreateEntity(props.plane.id);
     this.entity.model = props.planeModel;
-    this.entity.position = Cartesian3.fromDegrees(props.plane.lon, props.plane.lat, 5000);
+    // this.entity.position = Cartesian3.fromDegrees(
+    //   props.plane.lon,
+    //   props.plane.lat,
+    //   props.plane.altitude
+    // );
+    // const hpr = new HeadingPitchRoll(props.plane.heading, 0, 0);
+    // this.entity.orientation = Transforms.headingPitchRollQuaternion(this.entity.position, hpr);
   }
-
 
   componentDidMount() {
     // const { entityCollection } = this.props;
@@ -31,10 +35,15 @@ export default class CesiumEntity extends Component {
   }
 
   updateEntity() {
-    if (this.entity) {
-      this.entity.position = Cartesian3.fromDegrees(this.props.plane.lon, this.props.plane.lat, 5000);
-    }
-    // console.log(this.entity);
+    const position = Cartesian3.fromDegrees(
+      this.props.plane.lon,
+      this.props.plane.lat,
+      this.props.plane.altitude
+    );
+    this.entity.position = position
+    const heading = CesiumMath.toRadians(this.props.plane.heading);
+    const hpr = new HeadingPitchRoll(heading, 0, 0);
+    this.entity.orientation = Transforms.headingPitchRollQuaternion(position, hpr);
   }
 
   render() {
