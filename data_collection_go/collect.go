1667aaa1a09b72794583e9a41f7ac8035f8a1c75
Shakeel Saleemi <shakahs@gmail.com>
2018-01-13 15:39:12 -0800

    Implement validation of data in go scraper

--- data_collection_go/collect.go
+++ data_collection_go/collect.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"encoding/json"
-	//"github.com/asaskevich/govalidator"
+	"strconv"
+	"github.com/asaskevich/govalidator"
 )
 
-type adsb_record struct {
-	Id string `json:"Icao"`
-	Lat float64
-	Lng float64 `json:"Long"`
-	Time int64 `json:"PosTime"`
-	Heading float32 `json:"Trak"`
-	Altitude int64 `json:"Galt"`
+type adsb_record struct { //altitude is in feet
+	Id       string `json:"Icao"`
+	Lat      float64
+	Lng      float64 `json:"Long"`
+	Time     int64   `json:"PosTime"`
+	Heading  float32 `json:"Trak"`
+	Altitude int   `json:"Galt"`
 }
 
 type adsb_list = []adsb_record
@@ -40,7 +41,32 @@ func getAdsbData() adsb_list {
 	return unmarshaledData.AcList
 }
 
+func validateAdsbData(record adsb_record) bool {
+	//Icao id
+	return govalidator.IsAlphanumeric(record.Id) &&
+		govalidator.IsByteLength(record.Id, 3, 10) &&
+		// latitude
+		record.Lat != 0 &&
+		govalidator.IsLatitude(strconv.FormatFloat(record.Lat, 'f', -1, 64)) &&
+		// longitude
+		record.Lng != 0 &&
+		govalidator.IsLongitude(strconv.FormatFloat(record.Lng, 'f', -1, 64)) &&
+		// heading
+		govalidator.InRangeFloat32(record.Heading, 0, 360) &&
+		// altitude
+		govalidator.InRangeInt(record.Altitude, -1000, 100000)
+
+}
+
 func main() {
 	flightData := getAdsbData()
-	fmt.Print(flightData)
+	var validData []adsb_record
+
+	for _, val := range flightData {
+		if validateAdsbData(val) {
+			validData = append(validData, val)
+		}
+	}
+
+	fmt.Print(validData)
 }
