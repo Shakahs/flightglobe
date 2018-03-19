a3f9778d2a1824e37404a31fb5799d46da6bc8ba
Shakeel Saleemi <shakahs@gmail.com>
2018-03-18 21:38:56 -0700

    Minor cleanup of ADSB Exchange collection code, move types to their own file

--- dataserver/lib/adsbexchange/clean.go
+++ dataserver/lib/adsbexchange/clean.go
@@ -10,21 +10,6 @@ import (
 	"time"
 )
 
-type adsbRecord struct { //altitude is in feet
-	Icao    string
-	Lat     float64
-	Long    float64
-	PosTime int64 //timestamp with nanosecond
-	Trak    float64
-	Galt    float64 //altitude in feet
-}
-
-type adsbList = []adsbRecord
-
-type AdsbFeed struct {
-	AcList adsbList
-}
-
 func getRawAdsbData(resp []byte) adsbList {
 	var unmarshaledData AdsbFeed
 	err := json.Unmarshal(resp, &unmarshaledData)
