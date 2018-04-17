a2a5e75585f60870baa7ba6cabf5e934c3b19778
Shakeel Saleemi <shakahs@gmail.com>
2018-04-16 22:44:57 -0700

    Remover Adminer from Docker-Compose to save RAM in dev environment. Update FR Scraper URL template to exclude aircraft on ground.

--- dataserver/lib/flightradar24/constants.go
+++ dataserver/lib/flightradar24/constants.go
@@ -1,4 +1,4 @@
 package flightradar24
 
 const URL_TEMPLATE = "https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=%f,%f,%f,%f" +
-	"&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=0&estimated=1&maxage=14400&gliders=1&stats=0"
+	"&faa=1&mlat=1&flarm=1&adsb=1&gnd=0&air=1&vehicles=0&estimated=1&maxage=14400&gliders=1&stats=0"
