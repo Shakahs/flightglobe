9fb827b2f8bae90733e72eedc5a99fddc36ab650
Shakeel Saleemi <shakahs@gmail.com>
2018-08-19 23:07:05 -0700

    Revert plane object from using a Cesium SampledPosition to using a regular Position.

--- client-source/js/plane.js
+++ client-source/js/plane.js
@@ -13,24 +13,30 @@ export default class PlaneObj extends Object {
     this.data = null;
     this.entity = null;
 
-    this.sampledPosition = new SampledPositionProperty();
-    this.sampledPosition.forwardExtrapolationType = ExtrapolationType.HOLD;
-    this.sampledPosition.backwardExtrapolationType = ExtrapolationType.HOLD;
+    // this.sampledPosition = new SampledPositionProperty();
+    // this.sampledPosition.forwardExtrapolationType = ExtrapolationType.HOLD;
+    // this.sampledPosition.backwardExtrapolationType = ExtrapolationType.HOLD;
+    // this.sampledPosition.addSample(date, position);
 
-    this.updatePosition = this.updatePosition.bind(this);
+    // this.updatePosition = this.updatePosition.bind(this);
 
-    this.sampledPosition.addSample(date, position);
+    // this.sampledPosition.addSample(date, position);
+    
     this.entity = this.planeData.entities.add({
       point: {
         pixelSize: 2,
       },
-      position: this.sampledPosition,
+      // position: this.sampledPosition,
+      position,
       scaleByDistance: nfScalar,
       id: this.icao,
     });
+
+    this.updatePosition = this.updatePosition.bind(this);
   }
 
   updatePosition(position, date) {
-    this.sampledPosition.addSample(date, position);
+    // this.sampledPosition.addSample(date, position);
+    this.entity.position = position;
   }
 }
