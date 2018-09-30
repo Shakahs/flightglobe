37fd10aed081f967a848d73c8fe533b65ac64b10
Shakeel Saleemi <shakahs@gmail.com>
2018-09-29 19:59:53 -0700

    Gofmt file.

--- dataserver/cmd/fr-persistor/main.go
+++ dataserver/cmd/fr-persistor/main.go
@@ -21,19 +21,19 @@ var reJsonClient = rejonson.ExtendClient(goRedisClient)
 var redisSubChannel = os.Getenv("REDIS_SUB_CHANNEL")
 var redisDataKey = os.Getenv("REDIS_DATA_KEY")
 
-func checkKeyExists(){
+func checkKeyExists() {
 	if reJsonClient.Exists(redisDataKey).Val() == 0 {
-		_, err := reJsonClient.JsonSet(redisDataKey,".","{}").Result()
+		_, err := reJsonClient.JsonSet(redisDataKey, ".", "{}").Result()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("Key",redisDataKey, "did not exist, was created")
+		fmt.Println("Key", redisDataKey, "did not exist, was created")
 	} else {
 		fmt.Println("Confirmed that key", redisDataKey, "exists")
 	}
 }
 
-func persist(){
+func persist() {
 	pubsub := reJsonClient.Subscribe(redisSubChannel)
 	ch := pubsub.Channel()
 
@@ -44,26 +44,26 @@ func persist(){
 
 	for {
 		select {
-			case msg, ok := <-ch:
-				if !ok {
-					break
-				}
+		case msg, ok := <-ch:
+			if !ok {
+				break
+			}
 
-				//deserialize so we can get the ICAO.
-				var pos lib.Position
-				err := json.Unmarshal([]byte(msg.Payload), &pos) //get msg string, convert to byte array for unmarshal
-				if err != nil {
-					log.Fatal("unmarshal error", err)
-				}
+			//deserialize so we can get the ICAO.
+			var pos lib.Position
+			err := json.Unmarshal([]byte(msg.Payload), &pos) //get msg string, convert to byte array for unmarshal
+			if err != nil {
+				log.Fatal("unmarshal error", err)
+			}
 
-				//only persist if we have an ICAO, persisting an empty ICAO erases the ReJSON container
-				if pos.Icao != "" {
-					reJsonClient.JsonSet(redisDataKey, fmt.Sprintf(".%s", pos.Icao), msg.Payload)
-					persistedCount++
-				}
-			case <-ticker.C:
-				fmt.Println(persistedCount, "positions saved in past 5 seconds")
-				persistedCount = 0
+			//only persist if we have an ICAO, persisting an empty ICAO erases the ReJSON container
+			if pos.Icao != "" {
+				reJsonClient.JsonSet(redisDataKey, fmt.Sprintf(".%s", pos.Icao), msg.Payload)
+				persistedCount++
+			}
+		case <-ticker.C:
+			fmt.Println(persistedCount, "positions saved in past 5 seconds")
+			persistedCount = 0
 		}
 	}
 }
@@ -88,4 +88,3 @@ func main() {
 		}
 	}
 }
-
