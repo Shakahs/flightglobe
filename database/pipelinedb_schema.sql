975ce317a6766eb13c5c59f092d45ad77fa98f49
Shakeel Saleemi <shakahs@gmail.com>
2018-09-29 21:51:47 -0700

    Progress on PipelineDB schema for incremental flight track calculation.

--- database/pipelinedb_schema.sql
+++ database/pipelinedb_schema.sql
@@ -6,3 +6,12 @@ CREATE STREAM position_stream (
 	heading              real  NOT NULL,
 	altitude             integer  NOT NULL
 );
+
+CREATE CONTINUOUS VIEW active_flights WITH (sw = '10 minutes') as
+SELECT icao
+from position_stream
+
+CREATE CONTINUOUS VIEW flight_tracks as
+SELECT DISTINCT ON (icao, ptime3) icao, lat, lng,
+date_round(ptime,'30 seconds') AS ptime3
+FROM position_stream
