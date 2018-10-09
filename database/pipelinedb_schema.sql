ed850882794ad7655302b63dcf43d9545dcc241c
Shakeel Saleemi <shakahs@gmail.com>
2018-10-08 22:36:51 -0700

    Remove pushtosidecar.py script we will not be using. Make progress on figuring out PipelineDB continuous views.

--- database/pipelinedb_schema.sql
+++ database/pipelinedb_schema.sql
@@ -12,6 +12,26 @@ SELECT icao
 from position_stream
 
 CREATE CONTINUOUS VIEW flight_tracks as
-SELECT DISTINCT ON (icao, ptime3) icao, lat, lng,
-date_round(ptime,'30 seconds') AS ptime3
+SELECT DISTINCT ON (icao, ptime) icao, lat, lng,
+date_round(ptime,'30 seconds') AS ptime
 FROM position_stream;
+
+CREATE TABLE flight_tracks_output  (
+	id                   bigserial  NOT NULL,
+  icao                 char(6)  NOT NULL,
+-- 	ptime                timestamptz NOT NULL,
+	lat                  real  NOT NULL,
+	lng                  real  NOT NULL,
+	heading              real  NOT NULL,
+	altitude             integer  NOT NULL
+);
+CREATE INDEX idx_flight_tracks_output_id ON flight_tracks_output ( id );
+
+CREATE CONTINUOUS TRANSFORM flight_tracks_transform AS
+  SELECT icao::char(6), lat::real, lng::real
+   FROM output_of(flight_tracks)
+  THEN EXECUTE PROCEDURE insert_track_position();
+
+CREATE CONTINUOUS VIEW test as
+SELECT (new).icao::char(6), (new).lat::real, (new).lng::real
+   FROM output_of('flight_tracks');
