c01b8254df0c876dcb6066359b4faf9ffd472650
Shakeel Saleemi <shakahs@gmail.com>
2018-10-08 23:30:42 -0700

    Working version of PipelineDB continuous view + transform + trigger to populate a flight track export table for streaming to Redis.

--- database/pipelinedb_schema.sql
+++ database/pipelinedb_schema.sql
@@ -7,27 +7,24 @@ CREATE STREAM position_stream (
 	altitude             integer  NOT NULL
 );
 
-CREATE CONTINUOUS VIEW active_flights WITH (sw = '10 minutes') as
-SELECT icao
-from position_stream
-
 CREATE CONTINUOUS VIEW flight_tracks_aggregated as
-SELECT DISTINCT ON (icao, ptime_bucket) icao, lat, lng,
-date_round(ptime,'30 seconds') AS ptime_bucket
+SELECT DISTINCT ON (icao, ptime_bucket) icao, date_round(ptime,'30 seconds') AS ptime_bucket,
+lat, lng, heading, altitude
 FROM position_stream;
 
 CREATE TABLE flight_tracks_export  (
 	id                   bigserial  NOT NULL,
   icao                 char(6)  NOT NULL,
--- 	ptime                timestamptz NOT NULL,
+	ptime                timestamptz NOT NULL,
 	lat                  real  NOT NULL,
-	lng                  real  NOT NULL
--- 	heading              real  NOT NULL,
--- 	altitude             integer  NOT NULL
+	lng                  real  NOT NULL,
+	heading              real  NOT NULL,
+	altitude             integer  NOT NULL
 );
 CREATE INDEX idx_flight_tracks_export_id ON flight_tracks_export ( id );
 
 CREATE CONTINUOUS TRANSFORM flight_tracks_exporter as
-SELECT (new).icao::char(6), (new).lat::real, (new).lng::real
+SELECT (new).icao::char(6), (new).ptime_bucket::timestamptz as ptime, (new).lat::real, (new).lng::real,
+ (new).heading::real, (new).altitude::real
 FROM output_of('flight_tracks_aggregated')
 THEN EXECUTE PROCEDURE insert_track_position();
