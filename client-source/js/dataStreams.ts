4a0037b06990fd4b33c5177ec54c7eb5d2c17080
Shakeel Saleemi <shakahs@gmail.com>
2018-10-12 17:43:30 -0700

    WebSocket Observable now working with RXJS 6.

--- client-source/js/dataStreams.ts
+++ client-source/js/dataStreams.ts
@@ -1,22 +1,10 @@
-import { Observable } from 'rxjs';
-import {webSocket} from "rxjs/webSocket";
-import {map, share} from 'rxjs/operators'
+import { webSocket} from "rxjs/webSocket";
+
 import { globe } from './api';
-import {FlightPosition, FlightPositionMap} from "./types";
+import {FlightPositionMap} from "./types";
 
 const loc = window.location;
 
-// const wsStream$ = new Observable((observer) => {
-//   const socket = new WebSocket();
-//   socket.addEventListener('message', (e:MessageEvent) => observer.next(e.data));
-//   return () => socket.close();
-// });
+const socket$ =  webSocket<FlightPositionMap>(`ws://${ loc.host }/sub`);
 
-let wsStream$ = webSocket<string>(`ws://${ loc.host }/sub`);
-
-const wsStreamShare$ = wsStream$
-    .pipe(map<string, FlightPositionMap>((data) => JSON.parse(data)));
-    // .pipe(share());
-  // .share();
-
-export default wsStreamShare$;
+export default socket$;
