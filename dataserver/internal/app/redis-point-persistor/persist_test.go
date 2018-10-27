3d4389ee3b67f9be3a3165423c21e07b15ee20a2
Shakeel Saleemi <shakahs@gmail.com>
2018-10-26 19:40:39 -0700

    Add tests for identical Positions filter in Redis Persistor.

--- dataserver/internal/app/redis-point-persistor/persist_test.go
+++ dataserver/internal/app/redis-point-persistor/persist_test.go
@@ -16,10 +16,43 @@ func TestIsPositionValid(t *testing.T) {
 	}
 
 	for _, entry := range testTable {
-		actualResult := IsPositionValid(entry.pos)
+		actualResult := isPositionValid(entry.pos)
 		if actualResult != entry.expectedResult {
 			t.Errorf("Validation result of %s incorrect, got: %t, want: %t.",
 				entry.pos.Icao, actualResult, entry.expectedResult)
+		}
+	}
+}
+
+func TestArePositionsIdentical(t *testing.T) {
+
+	testTable := []struct {
+		oldPos         pkg.Position
+		newPos         pkg.Position
+		expectedResult bool
+	}{
+		{
+			oldPos:         pkg.Position{Latitude: 99, Longitude: 99},
+			newPos:         pkg.Position{Latitude: 99, Longitude: 99},
+			expectedResult: true,
+		},
+		{
+			oldPos:         pkg.Position{Latitude: 99, Longitude: 99},
+			newPos:         pkg.Position{Latitude: 98, Longitude: 99},
+			expectedResult: false,
+		},
+		{
+			oldPos:         pkg.Position{Latitude: 99, Longitude: 99},
+			newPos:         pkg.Position{Latitude: 99, Longitude: 98},
+			expectedResult: false,
+		},
+	}
+
+	for _, entry := range testTable {
+		actualResult := arePositionsIdentical(entry.oldPos, entry.newPos)
+		if actualResult != entry.expectedResult {
+			t.Errorf("Identical position result of incorrect, got: %t, want: %t.",
+				actualResult, entry.expectedResult)
 		}
 	}
 }
