112ce926492db766f060ba7092d77aefd38a52a1
Shakeel Saleemi <shakahs@gmail.com>
2018-10-26 19:06:24 -0700

    Progress on Position validation deduplication and filtering.

--- dataserver/internal/app/redis-point-persistor/persist.go
+++ dataserver/internal/app/redis-point-persistor/persist.go
@@ -5,62 +5,128 @@ import (
 	"fmt"
 	"github.com/KromDaniel/rejonson"
 	"github.com/Shakahs/flightglobe/dataserver/internal/pkg"
+	"github.com/pkg/errors"
 	"log"
 	"time"
 )
 
-func shouldSaveTrack(track pkg.Positions, new pkg.Position) bool {
+func isPositionValid(newPos pkg.Position) bool {
+	if newPos.Icao == "" {
+		return false
+	} //blank ICAO
+
+	return true
+}
+
+func arePositionsIdentical(oldPos pkg.Position, newPos pkg.Position) bool {
+	if oldPos.Latitude == newPos.Latitude && oldPos.Longitude == newPos.Longitude {
+		return true
+	} //identical coordinates
+
+	return false
+}
+
+func shouldSavePosition(oldPos pkg.Position, newPos pkg.Position) bool {
+	if !isPositionValid(newPos) {
+		return false
+	} //failed validation
+
+	if arePositionsIdentical(oldPos, newPos) {
+		return false
+	} //identical coordinates
+	return true
+}
+
+func shouldSaveTrackPosition(track pkg.Positions, newPos pkg.Position) bool {
+	if !isPositionValid(newPos) {
+		return false
+	} //failed validation
+
 	if len(track) == 0 {
 		return true
 	} //empty track
-	old := track[len(track)-1] //get last recorded position
-	if old.Latitude == new.Latitude && old.Longitude == new.Longitude {
+
+	oldPos := track[len(track)-1] //get last recorded position
+
+	if arePositionsIdentical(oldPos, newPos) {
 		return false
-	} //identical coordinates
-	delta := new.Time.Sub(old.Time)
+	} //identical positions
+
+	delta := newPos.Time.Sub(oldPos.Time)
 	if delta.Minutes() < 1 {
 		return false
 	} //less than 1 minute difference
 	return true
 }
 
-func persistLatestPosition(c *rejonson.Client, redisDataKey string, pos pkg.Position, rawPos string) error {
-	_, err := c.JsonSet(redisDataKey, fmt.Sprintf(".$%s", pos.Icao), rawPos).Result()
-	return err
+func persistLatestPosition(c *rejonson.Client, redisDataKey string, rawPos string) (bool, error) {
+	var newPos pkg.Position
+	err := json.Unmarshal([]byte(rawPos), &newPos) //get msg string, convert to byte array for unmarshal
+	if err != nil {
+		log.Fatal("unmarshal error", err)
+	}
+
+	JSONPath := fmt.Sprintf(".$%s", newPos.Icao)
+	var oldPos pkg.Position
+
+	currentPosRaw, err := c.JsonGet(redisDataKey, JSONPath).Bytes()
+	if err != nil {
+		return false, errors.New("Could not retrieve existing position:" + err.Error())
+	}
+
+	err = json.Unmarshal(currentPosRaw, &oldPos)
+	if err != nil {
+		return false, errors.New("Could not unmarshal existing position:" + err.Error())
+	}
+
+	if shouldSavePosition(oldPos, newPos) {
+		_, err = c.JsonSet(redisDataKey, JSONPath, rawPos).Result()
+		if err != nil {
+			return false, errors.New("Could not save new position:" + err.Error())
+		}
+	}
+
+	return true, nil
 }
 
-func persistTrack(c *rejonson.Client, pos pkg.Position, rawPos string) error {
-	trackKey := fmt.Sprintf("track:$%s", pos.Icao)
-	_, err := pkg.EnsureJSONKeyExists(c, trackKey, "[]")
+func persistLatestTrackPosition(c *rejonson.Client, rawPos string) (bool, error) {
+	var newPos pkg.Position
+	err := json.Unmarshal([]byte(rawPos), &newPos) //get msg string, convert to byte array for unmarshal
 	if err != nil {
-		return err
+		log.Fatal("unmarshal error", err)
+	}
+
+	trackKey := fmt.Sprintf("track:$%s", newPos.Icao)
+	_, err = pkg.EnsureJSONKeyExists(c, trackKey, "[]")
+	if err != nil {
+		return false, err
 	}
 
 	var track pkg.Positions
 	rawTrack, err := c.JsonGet(trackKey).Bytes()
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	err = json.Unmarshal(rawTrack, &track)
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	if shouldSaveTrack(track, pos) { //compare last element of existing track to this new element
+	if shouldSaveTrackPosition(track, newPos) { //compare last element of existing track to this new element
 		_, err = c.JsonArrAppend(trackKey, ".", rawPos).Result()
 		if err != nil {
-			return err
+			return false, err
 		}
 	}
 
 	expireAt, err := time.ParseDuration("10m")
 	_, err = c.Expire(trackKey, expireAt).Result()
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	return nil
+	return true, nil
 }
 
 //remove stale positions from our JSON map of current positions
@@ -87,8 +153,8 @@ func Persist(c *rejonson.Client, redisSubChannel string, redisDataKey string) {
 	ticker := time.NewTicker(time.Second * 5)
 	defer ticker.Stop()
 
-	persistedCount := 0
-	droppedCount := 0
+	persistedPositionCount := 0
+	droppedPositionCount := 0
 
 	for {
 		select {
@@ -97,33 +163,25 @@ func Persist(c *rejonson.Client, redisSubChannel string, redisDataKey string) {
 				break
 			}
 
-			//deserialize so we can get the ICAO.
-			var pos pkg.Position
-			err := json.Unmarshal([]byte(msg.Payload), &pos) //get msg string, convert to byte array for unmarshal
+			wasPointPersisted, err := persistLatestPosition(c, redisDataKey, msg.Payload)
 			if err != nil {
-				log.Fatal("unmarshal error", err)
+				log.Println("Encountered an error saving a position:", err)
 			}
 
-			//only persist if we have an ICAO, persisting an empty ICAO erases the ReJSON container
-			if pos.Icao != "" {
-				err := persistLatestPosition(c, redisDataKey, pos, msg.Payload)
-				if err != nil {
-					log.Println("Encountered an error saving position for ", pos.Icao)
-				}
+			_, err = persistLatestTrackPosition(c, msg.Payload)
+			if err != nil {
+				log.Println("Encountered an error saving a track:", err)
+			}
 
-				err = persistTrack(c, pos, msg.Payload)
-				if err != nil {
-					log.Println("Encountered an error saving track for ", pos.Icao)
-				}
-
-				persistedCount++
+			if wasPointPersisted {
+				persistedPositionCount++
 			} else {
-				droppedCount++
+				droppedPositionCount++
 			}
 		case <-ticker.C:
-			log.Println(persistedCount, "positions saved,", droppedCount, "positions dropped in past 5 seconds")
-			persistedCount = 0
-			droppedCount = 0
+			log.Println(persistedPositionCount, "positions saved,", droppedPositionCount, "positions dropped in past 5 seconds")
+			persistedPositionCount = 0
+			droppedPositionCount = 0
 
 			deleted, err := cleanStalePositions(c, redisDataKey)
 			if err != nil {
