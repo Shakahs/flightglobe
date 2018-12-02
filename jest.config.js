10d471c84af24a160819bb3a2c0a5fed91b878aa
Shak <shakahs@gmail.com>
2018-12-01 20:46:15 -0800

    Setup Jest code coverage reporting.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['client-source'],
+  collectCoverage: true,
+  'collectCoverageFrom': [
+    '**/*.{ts,tsx}',
+    '!**/js_legacy*/**',
+  ],
 };
