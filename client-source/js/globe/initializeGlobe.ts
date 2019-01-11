b8d79220cf631e048f811f7eb02449337309f817
Shak <shakahs@gmail.com>
2019-01-11 15:56:12 -0800

    Setup Bing imagery provider.

--- client-source/js/globe/initializeGlobe.ts
+++ client-source/js/globe/initializeGlobe.ts
@@ -10,7 +10,7 @@ import * as Cesium from "cesium";
 // loadAirports(airportData, airportDataRaw);
 
 //@ts-ignore. This is not in the Cesium TypeScript definitions yet
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0OThjM2RiYS02MjA0LTQ1MDEtYjU2Ni00MzY2NjlkY2Q1ODMiLCJpZCI6NjQxMywic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0NjQ3NzQ1NX0.Z85-lIf_qTbJ2z2FcSez-bDeVHJ_H9u_OjxPFO16ios';
+// Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0OThjM2RiYS02MjA0LTQ1MDEtYjU2Ni00MzY2NjlkY2Q1ODMiLCJpZCI6NjQxMywic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0NjQ3NzQ1NX0.Z85-lIf_qTbJ2z2FcSez-bDeVHJ_H9u_OjxPFO16ios';
 
 const viewer = new Cesium.Viewer('cesiumContainer', {
     animation: false,
@@ -31,13 +31,18 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
     // targetFrameRate: 30
     // shouldAnimate: true,
     // automaticallyTrackDataSourceClocks: false,
+    imageryProvider : new Cesium.BingMapsImageryProvider({
+        url: 'http://dev.virtualearth.net',
+        key: 'Annkf_qhmEYkCSYC9PrxYrFwP1ZJOH1wm1x_g5GeuoXkWII7RU94Npx8VJUFwDMZ',
+        mapStyle: 'AerialWithLabels'
+    }),
 });
 
-viewer.imageryLayers.addImageryProvider(
-    new Cesium.IonImageryProvider({
-        assetId: 3,
-    })
-);
+// viewer.imageryLayers.addImageryProvider(
+//     new Cesium.IonImageryProvider({
+//         assetId: 3,
+//     })
+// );
 
 // viewer.camera.defaultZoomAmount = 1000000.0;
 // viewer.screenSpaceEventHandler.setInputAction(
