584a61a46a5eec882b59dfbb34e2eea547754cba
Shak <shakahs@gmail.com>
2019-01-11 14:19:42 -0800

    Fix Webpack production build config.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,7 @@
 const devConfig = require('./webpack.config');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 devConfig.devtool = undefined;
-devConfig.optimization = {
-  minimizer: [new UglifyJsPlugin()],
-};
+devConfig.devServer = undefined;
+devConfig.mode = 'production';
 
 module.exports = devConfig;
