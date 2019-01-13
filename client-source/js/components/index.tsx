dbc9b67e917185b1d9dcca2b3115bce5b0f0c528
Shak <shakahs@gmail.com>
2019-01-12 19:40:10 -0800

    Remove unneeded code.

--- client-source/js/components/index.tsx
+++ client-source/js/components/index.tsx
@@ -1,12 +1,9 @@
 import "@babel/polyfill";
 import 'cesiumSource/Widgets/widgets.css';
-import {buffered$, socket$} from '../api/webSocket';
 import {viewer} from '../globe';
-import {forEach, size} from 'lodash-es'
 import {interval} from 'rxjs';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import {DemographicsUpdate, PositionUpdate} from "../types";
 import {FlightStore} from '../flightStore'
 import applyClickHandler from "../globe/clickHandler";
 import App from "./app";
@@ -19,39 +16,9 @@ applyClickHandler(viewer, flightStore)
 
 const pollInterval = interval(5000);
 pollInterval.subscribe(()=>{
-  // console.log("asking for positions after", newestPositionTime)
   // @ts-ignore: we need to send a request here, not a FlightPosition
   wsh.send({lastReceivedTimestamp: flightStore.newestPositionTimestamp})
 });
-
-// buffered$.subscribe((messages) => {
-//     console.log(size(messages), "messages received");
-//
-//     // const affectedGeos = new Set();
-//     forEach(messages, (message)=>{
-//       switch (message.type) {
-//           case "positionUpdate":
-//             const pUpdate = message as PositionUpdate;
-//             flightStore.addOrUpdateFlight(pUpdate);
-//             break;
-//           case "demographicUpdate":
-//             const dUpdate = message as DemographicsUpdate;
-//             flightStore.addDemographics(dUpdate);
-//             break;
-//       }
-//     });
-//
-//     viewer.scene.requestRender();
-//
-//     // console.log(`${affectedGeos.size} geohashed datasources affected`);
-//     // affectedGeos.clear();
-// });
-
-setInterval(()=>{
-    console.log(`${flightStore.numberFlights()} flights in memory`)
-    console.log(`${flightStore.numberGeos()} geohash datasources`)
-    // console.log(flightData)
-}, 15000);
 
 ReactDOM.render(
     <App
