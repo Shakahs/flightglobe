c3117a8a3747ea596704e7468de83118fdf9540c
Shak <shakahs@gmail.com>
2019-01-27 21:08:01 -0800

    Created a script to automate gcloud build submissions.

---
+++ buildimages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcloud builds submit --tag gcr.io/flight-globe/redis-point-persistor dataserver/cmd/redis-point-persistor --async
+gcloud builds submit --tag gcr.io/flight-globe/fr-collector dataserver/cmd/fr-collector --async
+gcloud builds submit --tag gcr.io/flight-globe/fg-server dataserver/cmd/fg-server --async
+
