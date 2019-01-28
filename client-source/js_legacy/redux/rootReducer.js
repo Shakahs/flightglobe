2ee8f48451cf238900a4819ddcbf3c7116cdfe8a
Shak <shakahs@gmail.com>
2019-01-28 00:36:47 -0800

    Remove legacy code.

--- client-source/js_legacy/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux-immutable';
-import { reducer as globeReducer } from './globe';
-
-const reducers = {
-  globe: globeReducer,
-};
-
-export default combineReducers(reducers);
--- client-source/js_legacy/redux/rootSaga.js
+++
@@ -1,9 +0,0 @@
-import { fork, all } from 'redux-saga/effects';
-import { map, merge } from 'lodash-es';
-import { sagas as globeSagas } from './globe';
-
-const allSagas = merge({}, globeSagas);
-
-export default function* () {
-  yield all(map(allSagas, fork));
-}
