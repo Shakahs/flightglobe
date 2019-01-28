813fb005b678bd7aad6e9b5a0dace38edf1fdbf9
Shak <shakahs@gmail.com>
2019-01-27 21:58:17 -0800

    Enable content hashed filenaming for Webpack bundle output.

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     ],
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dataserver/cmd/fg-server/static'),
     // Needed by Cesium for multiline strings
     sourcePrefix: '',
