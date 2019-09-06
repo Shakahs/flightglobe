030ddb9e79d4ef8cd6059bf0eeceac0c84096e09
Shakeel Saleemi <shakahs@gmail.com>
2019-09-06 10:50:23 -0700

    Add a script to build the golang executables.

---
+++ buildexecutables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+( cd dataserver/cmd/fr-collector && go get -v && go build -v )
+( cd dataserver/cmd/redis-point-persistor && go get -v && go build -v )
+( cd dataserver/cmd/fg-server && go get -v && go build -v )
