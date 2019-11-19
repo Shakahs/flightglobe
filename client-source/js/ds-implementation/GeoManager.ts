00a7decc9c8e132df9c95f1ecaccc9495965a400
Shakeel Saleemi <shakahs@gmail.com>
2019-11-19 10:47:32 -0800

    Remove the MobX observable flightPositions Map in GeoManager, instead of updating positions here we will update positions on each of the FlightSubscribers individually.

--- client-source/js/ds-implementation/GeoManager.ts
+++ client-source/js/ds-implementation/GeoManager.ts
@@ -3,17 +3,17 @@ import { FlightPosition, Geohash, GeoPositionList } from "../../../lib/types";
 import { ObservableMap } from "mobx";
 import { Icao } from "../types";
 import { FlightSubscriber } from "./FlightSubscriber";
-import { without } from "lodash-es";
+import { without, forEach, keys } from "lodash";
 
 export class GeoManager {
    dsConn: DeepstreamClient;
    dsRecord;
    geohash: Geohash;
-   flightPositions = new ObservableMap<Icao, FlightPosition>(
-      undefined,
-      undefined,
-      "flightPositions"
-   );
+   // flightPositions = new ObservableMap<Icao, FlightPosition>(
+   //    undefined,
+   //    undefined,
+   //    "flightPositions"
+   // );
    flightSubscriberMap: Map<Icao, FlightSubscriber>;
 
    constructor(dsConn: DeepstreamClient, geohash: Geohash) {
@@ -28,12 +28,15 @@ export class GeoManager {
    }
 
    handleUpdate(update: GeoPositionList) {
-      this.flightPositions.replace(update.flights);
+      // this.flightPositions.replace(update.flights);
 
       //create new FlightSubscribers under the respective ICAO key
-      this.flightPositions.forEach((v, k) => {
-         if (!this.flightSubscriberMap.has(k)) {
-            this.flightSubscriberMap.set(k, new FlightSubscriber());
+      forEach(update.flights, (fpos, icao) => {
+         if (!this.flightSubscriberMap.has(icao)) {
+            this.flightSubscriberMap.set(
+               icao,
+               new FlightSubscriber(this.dsConn, icao, fpos, () => {})
+            );
          }
       });
 
@@ -42,7 +45,7 @@ export class GeoManager {
       // the resulting keys are flights that need to be deleted
       without(
          Array.from(this.flightSubscriberMap.keys()),
-         ...this.flightPositions.keys()
+         ...keys(update.flights)
       ).forEach((d) => {
          this.flightSubscriberMap.get(d)?.destroy();
          this.flightSubscriberMap.delete(d);
--- client-source/js/ds-implementation/spec/GeoManager.test.ts
+++ client-source/js/ds-implementation/spec/GeoManager.test.ts
@@ -21,29 +21,29 @@ describe("GeoManager", () => {
       gm = gmc.geoManagerMap.get("a") as GeoManager;
    });
 
-   it("should update the flightPositions map when it receives data", function() {
-      expect(gm.flightPositions.size).toEqual(0);
-      const fakeDataA = fakeFlightPosition();
-      const fakeDataB = fakeFlightPosition();
-      gm.handleUpdate({
-         geohash: gm.geohash,
-         flights: {
-            icaoA: fakeDataA,
-            icaoB: fakeDataB
-         }
-      });
-      expect(gm.flightPositions.size).toEqual(2);
-      expect(gm.flightPositions.get("icaoA")).toEqual(fakeDataA);
-      expect(gm.flightPositions.get("icaoB")).toEqual(fakeDataB);
-      gm.handleUpdate({
-         geohash: gm.geohash,
-         flights: {
-            icaoA: fakeDataA
-         }
-      });
-      expect(gm.flightPositions.size).toEqual(1);
-      expect(gm.flightPositions.get("icaoA")).toEqual(fakeDataA);
-   });
+   // it("should update the flightPositions map when it receives data", function() {
+   //    expect(gm.flightPositions.size).toEqual(0);
+   //    const fakeDataA = fakeFlightPosition();
+   //    const fakeDataB = fakeFlightPosition();
+   //    gm.handleUpdate({
+   //       geohash: gm.geohash,
+   //       flights: {
+   //          icaoA: fakeDataA,
+   //          icaoB: fakeDataB
+   //       }
+   //    });
+   //    expect(gm.flightPositions.size).toEqual(2);
+   //    expect(gm.flightPositions.get("icaoA")).toEqual(fakeDataA);
+   //    expect(gm.flightPositions.get("icaoB")).toEqual(fakeDataB);
+   //    gm.handleUpdate({
+   //       geohash: gm.geohash,
+   //       flights: {
+   //          icaoA: fakeDataA
+   //       }
+   //    });
+   //    expect(gm.flightPositions.size).toEqual(1);
+   //    expect(gm.flightPositions.get("icaoA")).toEqual(fakeDataA);
+   // });
 
    it("should create FlightSubscribers when needed", function() {
       expect(gm.flightSubscriberMap.size).toEqual(0);
