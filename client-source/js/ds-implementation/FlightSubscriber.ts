718555c46a9e0dcd1fc1e21612f6324434321534
Shakeel Saleemi <shakahs@gmail.com>
2019-11-19 20:42:10 -0800

    Commit initial work on FlightSubscriber.

--- client-source/js/ds-implementation/FlightSubscriber.ts
+++ client-source/js/ds-implementation/FlightSubscriber.ts
@@ -1,3 +1,51 @@
+import { DeepstreamClient } from "@deepstream/client";
+import { FlightPosition } from "../../../lib/types";
+import { Icao } from "../types";
+import { action, autorun, computed, IReactionDisposer, observable } from "mobx";
+import { Cartesian3 } from "cesium";
+import { convertPositionToCartesian } from "../ws-implementation/utility";
+require("./mobxConfig");
+
 export class FlightSubscriber {
+   private readonly dsConn: DeepstreamClient;
+   private readonly icao: Icao;
+   // gm: GeoManager;
+   // private disposers: Array<IReactionDisposer>;
+   @observable private position: FlightPosition;
+   private readonly requestRender: () => void;
+
+   constructor(
+      dsConn: DeepstreamClient,
+      icao: Icao,
+      pos: FlightPosition,
+      requestRender: () => void
+   ) {
+      this.dsConn = dsConn;
+      this.icao = icao;
+      // this.gm = gm;
+      this.position = pos;
+      this.requestRender = requestRender;
+
+      // const pointUpdater = autorun(
+      //    () => {
+      //       const newPosition = this.cartesianPosition;
+      //       if (newPosition) {
+      //          this.requestRender();
+      //       }
+      //    },
+      //    { name: "pointUpdater" }
+      // );
+      // this.disposers = [pointUpdater];
+   }
+
+   @action
+   updatePosition(newPos: FlightPosition) {
+      this.position = newPos;
+   }
+
+   @computed get cartesianPosition(): Cartesian3 {
+      return convertPositionToCartesian(this.position);
+   }
+
    destroy() {}
 }
