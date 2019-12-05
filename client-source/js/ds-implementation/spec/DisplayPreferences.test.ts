c9f68525a2f4782968fe2090f8a4b59ea07c5980
Shakeel Saleemi <shakahs@gmail.com>
2019-12-05 00:27:42 -0800

    Copy and adapt DisplayPreferences tests from the old FlightStore tests.

---
+++ client-source/js/ds-implementation/spec/DisplayPreferences.test.ts
@@ -0,0 +1,87 @@
+import {
+   LabelDisplayOptionDefaults,
+   PointDisplayOptionDefaults,
+   TrackDisplayOptionDefaults
+} from "../../constants";
+import {
+   LabelDisplayOptions,
+   LabelDisplayOptionsUpdate,
+   PointDisplayOptions,
+   PointDisplayOptionsUpdate,
+   TrackDisplayOptions,
+   TrackDisplayOptionsUpdate
+} from "../../types";
+import { cloneDeep, merge } from "lodash-es";
+import { Color } from "cesium";
+import { DisplayPreferences } from "../DisplayPreferences";
+
+describe("DisplayPreferences", function() {
+   let dp: DisplayPreferences;
+
+   beforeEach(() => {
+      dp = new DisplayPreferences();
+   });
+
+   describe("for points", function() {
+      it("should update the point display colors", function() {
+         expect(dp.pointDisplayOptions).toEqual(PointDisplayOptionDefaults);
+         const newOptions: PointDisplayOptionsUpdate = {
+            color: "#ff6699"
+         };
+         dp.updatePointDisplay(newOptions);
+         const finalResult = cloneDeep(PointDisplayOptionDefaults);
+         merge<PointDisplayOptions, PointDisplayOptionsUpdate>(
+            finalResult,
+            newOptions
+         );
+         expect(dp.pointDisplayOptions).toEqual(finalResult);
+         expect(
+            dp.pointDisplayOptions.cesiumColor.equals(
+               Color.fromCssColorString(newOptions.color as string)
+            )
+         ).toBeTruthy();
+      });
+   });
+
+   describe("for tracks", function() {
+      it("should update the track display options", function() {
+         expect(dp.trackDisplayOptions).toEqual(TrackDisplayOptionDefaults);
+         const newOptions = {
+            color: "#ff6699"
+         };
+         dp.updateTrackDisplay(newOptions);
+         const finalResult = cloneDeep(TrackDisplayOptionDefaults);
+         merge<TrackDisplayOptions, TrackDisplayOptionsUpdate>(
+            finalResult,
+            newOptions
+         );
+         expect(dp.trackDisplayOptions).toEqual(finalResult);
+         expect(
+            dp.trackDisplayOptions.cesiumColor.equals(
+               Color.fromCssColorString(newOptions.color)
+            )
+         ).toBeTruthy();
+      });
+   });
+
+   describe("for labels", function() {
+      it("should update the label display options", function() {
+         expect(dp.labelDisplayOptions).toEqual(LabelDisplayOptionDefaults);
+         const newOptions = {
+            color: "#ff6699"
+         };
+         dp.updateLabelDisplay(newOptions);
+         const finalResult = cloneDeep(LabelDisplayOptionDefaults);
+         merge<LabelDisplayOptions, LabelDisplayOptionsUpdate>(
+            finalResult,
+            newOptions
+         );
+         expect(dp.labelDisplayOptions).toEqual(finalResult);
+         expect(
+            dp.labelDisplayOptions.cesiumColor.equals(
+               Color.fromCssColorString(newOptions.color)
+            )
+         ).toBeTruthy();
+      });
+   });
+});
